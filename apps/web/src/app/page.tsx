59940d0c320a896eaa5ebcb5e486a02c97e2b602
Krishna <krishnachaudhary2007gt@gmail.com>
2026-09-18 10:23:24 +0530

    Add Button component to Home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,6 +7,7 @@ import FAQ from "@/components/FAQ";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
 import InputMic from "@/components/InputMic";
+import Button from "@/components/Buttons";
 
 
 export default function Home() {
@@ -21,7 +22,7 @@ export default function Home() {
         <FAQ />
         <CTA />
         
-        <InputMic />
+      
       </main>
       <Footer />
     </div>
